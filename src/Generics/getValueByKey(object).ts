fc7b799ccf2232c6ed7f8df1cdb702d9ac751263
IlyaAgarishev <ilyagarishev@gmail.com>
2022-06-18 20:20:22 +0500

    names changed

--- src/Generics/getValueByKey(object).ts
+++ src/Generics/getValueByKey(object).ts
@@ -1,5 +1,5 @@
-const getValue = <T extends object, U extends keyof T>(obj: T, prop: U) => {
+const getValueByKey = <T extends object, U extends keyof T>(obj: T, prop: U) => {
   return obj[prop];
 };
 
-const obj1 = getValue({ name: "Ilya", age: 23 }, "age");
+const obj1 = getValueByKey({ name: "Ilya", age: 23 }, "age");
